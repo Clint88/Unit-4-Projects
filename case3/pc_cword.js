1c7240de6921e11e90347b58b4c47d140450c37f
clint88 <ccrockett154@west-mec.org>
2020-04-02 22:49:17 -0700

    step 8 and 9

--- case3/pc_cword.js
+++ case3/pc_cword.js
@@ -59,21 +59,96 @@ window.onload = init();
 
 function init(){
    allLetters = document.querySelectorAll("table#crossword span");
+
    currentLetter = allLetters[0];
+
    var acrossID = currentLetter.dataset.clueA;
    var downID = currentLetter.dataset.clueD;
+
    acrossClue = document.getElementById(currentLetter.dataset.clueA);
    downClue = document.getElementById(currentLetter.dataset.clueD);
+
+   formatPuzzle(currentLetter){
+      for(var i = 0; i = allLetters.length; i++){
+         cursor.style = "pointer";
+         addEventListener("onmousedown", function(onmousedown){})
+      }
+   }
 }
 
-// function formatPuzzle(){
-//    currentLetter = puzzleLetter;
-//    for(var i = 0; i < allLetters.length; i++){
-//       allLetters[i].style.backgroundColor = "";
-//    }
-//    acrossClue[i]style.;
-// }
+function formatPuzzle(){
+   currentLetter = puzzleLetter;
+   for(var i = 0; i < allLetters.length; i++){
+      allLetters[i].style.backgroundColor = "";
+   }
+   acrossClue.style.color = "";
+   downClue.style.color = "";
 
+   if(currentLetter.dataset.clueA !== undefined){
+      acrossClue = document.getElementById(currentLetter.dataset.clueA);
+      acrossClue.style.color = "blue";
+
+      wordLetters = document.querySelectorAll("[data-clue-a =" + currentLetter.dataset.clueA + "]");
+     
+      wordLetters[0].style.backgroundColor = "rgb(231, 231, 255)";
+   }
+   if(currentLetter.dataset.clueD !== undefined){
+      acrossClue = document.getElementById(currentLetter.dataset.clueD);
+      acrossClue.style.color = "red";
+
+      wordLetters = document.querySelectorAll("[data-clue-d =" + currentLetter.dataset.clueD + "]");
+      
+      wordLetters[0].style.backgroundColor = "rgb(255, 231, 231)";
+   }
+   if(typeDirection === "right"){
+      currentLetter.style.backgroundColor = "rgb(191, 191, 255)";
+   }
+   else{
+      currentLetter.style.backgroundColor = "rgb(255, 191, 191)";
+   }
+}
+
+function selectLetter(){
+   var leftLetter = currentLetter.dataset.left;
+   var upLetter = currentLetter.dataset.up;
+   var rightLetter = currentLetter.dataset.right;
+   var downLetter = currentLetter.dataset.down;
+
+   // userKey = keycode;  ?
+
+   if(userKey = 37){
+      formatPuzzle(leftLetter);
+   }
+   if(userKey === 38){
+      formatPuzzle(upLetter);
+   }
+   if(userKey === 39 || userKey === 9){
+      formatPuzzle(rightLetter);
+   }
+   if(userKey === 40 || userKey === 13){
+      formatPuzzle(downLetter);
+   }
+   if(userKey === 8 || userKey === 46){
+      formatPuzzle(){
+         currentLetter = "";
+      }
+   }
+   if(userKey === 32){
+      swichTypeDirection();
+   }
+   if(userKey === 65 && userKey === 90){
+      currentLetter = getChar("userKey"){
+         if(typeDirection = "right"){
+            formatPuzzle(rightLetter);
+         }
+         else{
+            formatPuzzle(downLetter);
+         }
+      }
+   }
+   preventDefalt()
+}
+   
    
 
 
